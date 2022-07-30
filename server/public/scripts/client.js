d6fc1e834d0929ee75d6e76afa830ba6c8e71b5c
Jonathon Baxendell <j.bax626@gmail.com>
2022-07-30 12:57:03 -0500

    opperator capture mode success

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -13,13 +13,13 @@ function submitEquals () {
     // console.log('in submitEquals');
     // Take the input values and operator data and send
     // to the server
-
+    
     $.ajax({
         type: 'POST',
         url: '/equations',
         data: {
             numOne: $('.firstNum').val(),
-            opp: $('.opperator').val(),
+            opp: opperator,
             numTwo: $('.secondNum').val(),
         }
     }).then(function(response) {
@@ -28,16 +28,19 @@ function submitEquals () {
     });
 }
 
+let opperator = '';
+
 function getEquations () {
     $.ajax({
         type: 'GET',
         url: '/equations'
     }).then(function(response) {
+        $('#history').empty();
         for (let i = 0; i < response.length; i++) {
             let equals = response[i];
             $('#history').append(`
             <ul>
-                <li>${equals.numOne} ${equals.opp} ${equals.numTwo}
+                <li>${equals.numOne} ${equals.opp} ${equals.numTwo} </li>
             </ul>
         `)
         }
@@ -46,8 +49,6 @@ function getEquations () {
 
 function opperatorClick () {
     let opp = $(this).data('mode');
-
-    console.log(opp);
-
+    opperator = opp;
 }
 
